39a0a235062353734c6184fa314386d0e1205636
chrkk <christianjake.alalim@cit.edu>
2025-09-17 10:57:27 +0800

    added typewriter effect

---
+++ src/rpg/utils/TextEffect.java
@@ -0,0 +1,16 @@
+package rpg.util;
+
+public class TextEffects {
+
+    public static void typeWriter(String text, int delayMillis) {
+        for (char c : text.toCharArray()) {
+            System.out.print(c);
+            try {
+                Thread.sleep(delayMillis);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        System.out.println();
+    }
+}
