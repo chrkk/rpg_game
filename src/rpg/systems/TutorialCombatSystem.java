1d5f563034386fe5957f21c7dd9abafd056e6db1
chrkk <christianjake.alalim@cit.edu>
2025-10-28 01:29:10 +0800

    fixed shield take

--- src/rpg/systems/TutorialCombatSystem.java
+++ src/rpg/systems/TutorialCombatSystem.java
@@ -4,26 +4,21 @@ import java.util.Scanner;
 import rpg.utils.TextEffect;
 import rpg.characters.Player;
 import rpg.characters.Enemy;
-
-//new imports for meat consumable
 import rpg.items.Consumable;
 import rpg.game.GameState;
 
 public class TutorialCombatSystem {
     private Scanner scanner = new Scanner(System.in);
-    // private Random rand = new Random();
-    private GameState state; //new
+    private GameState state;
 
-    //new constuctor to rcv current gameState values
     public TutorialCombatSystem(GameState state) {
-        this.state = state; // store the shared game state
+        this.state = state;
     }
 
     public boolean startTutorialCombat(Player player, Enemy enemy) {
         TextEffect.typeWriter("⚔️ Tutorial Combat! " + enemy.getName() + " blocks your path!", 50);
 
         while (player.isAlive() && enemy.isAlive()) {
-            // --- Player Turn ---
             TextEffect.typeWriter("\nYour HP: " + player.getHp() + "/" + player.getMaxHp() +
                                   " | Mana: " + player.getMana() + "/" + player.getMaxMana() +
                                   " | Enemy HP: " + enemy.getHp(), 40);
@@ -39,7 +34,7 @@ public class TutorialCombatSystem {
                     int dmg = player.getWeapon().getDamage() + (player.getIntelligence() / 2);
                     enemy.takeDamage(dmg);
                     TextEffect.typeWriter("You strike with your " + player.getWeapon().getName() +
-                    " for " + dmg + " damage!", 40);
+                                          " for " + dmg + " damage!", 40);
                     break;
 
                 case "defend":
@@ -53,11 +48,10 @@ public class TutorialCombatSystem {
                         meat.consume(player, state);
                     } else {
                         TextEffect.typeWriter("You check your bag... but there's no item to consume!", 40);
-                     }
+                    }
                     break;
 
                 case "run":
-                    // In tutorial, running is disabled
                     TextEffect.typeWriter("You try to run... but there’s nowhere to escape!", 40);
                     break;
 
@@ -70,8 +64,8 @@ public class TutorialCombatSystem {
                 int dmgTaken = enemy.enemyAction();
                 if (defended) dmgTaken /= 2;
 
-                player.takeDamage(dmgTaken);
-                TextEffect.typeWriter("The " + enemy.getName() + " attacks! You take " + dmgTaken + " damage.", 40);
+                int actualDamage = player.takeDamage(dmgTaken);
+                TextEffect.typeWriter("The " + enemy.getName() + " attacks! You take " + actualDamage + " damage.", 40);
             }
         }
 
@@ -79,17 +73,18 @@ public class TutorialCombatSystem {
         if (player.isAlive()) {
             TextEffect.typeWriter("🎉 You defeated the " + enemy.getName() + "!", 50);
 
-            // EXP introduction
+            // EXP introduction + actual gain
             TextEffect.typeWriter("You feel a strange warmth flow through your body...", 70);
             TextEffect.typeWriter("[Narrator] This is experience. With enough battles, you will grow stronger and unlock new powers.", 70);
 
-            // Later, when Role 1 finishes EXP system:
-            // player.gainExp(enemy.getExpReward());
+            int expGained = enemy.getExpReward();
+            player.gainExp(expGained);
+            TextEffect.typeWriter("You gained " + expGained + " EXP!", 50);
 
             return true;
         } else {
             TextEffect.typeWriter("💀 You were defeated... but this is only training. You awaken again, determined.", 50);
-            player.healFull(); // respawn with full stats
+            player.healFull();
             return false;
         }
     }
