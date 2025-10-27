f793aacff70296a2a8b88de18b08367bc129b039
Ethan <ethanjohn.montejo@cit.edu>
2025-10-27 23:43:37 +0800

    a bit of changes in status system

--- src/rpg/systems/StatusSystem.java
+++ src/rpg/systems/StatusSystem.java
@@ -6,8 +6,8 @@ import rpg.utils.TextEffect;
 public class StatusSystem {
     public static void showStatus(Player player, int crystals, int meat) {
         TextEffect.typeWriter(
-            player.getName() + " the " + player.getTrait() +
-            "\n | HP: " + player.getHp() + "/" + player.getMaxHp() +
+            "\n------ " + player.getName() + ": The " + player.getTrait() + " ------" +
+            "\n| HP: " + player.getHp() + "/" + player.getMaxHp() +
             " | Level: " + player.getLevel() + // ---> new
             " | EXP: " + player.getExp() + "/" + player.getExpToNextLevel() + // ---> new
             " | Mana: " + player.getMana() + "/" + player.getMaxMana() +
