46efa3b545c8df129c90cb007ce28d4a47609f72
chrkk <christianjake.alalim@cit.edu>
2025-10-29 18:10:31 +0800

    added shards (currency) in gameState

--- src/rpg/game/GameState.java
+++ src/rpg/game/GameState.java
@@ -4,6 +4,7 @@ public class GameState {
     public int zone = 1;
     public int forwardSteps = 0;
     public int crystals = 0;
+    public int shards = 0;         
     public int meat = 0;
     public int revivalPotions = 0;
     public Enemy currentZoneBoss; 
