9af9bdc0c70059d1e00eabdc433f9abe2f0e429f
chrkk <christianjake.alalim@cit.edu>
2025-10-29 17:25:04 +0800

    added the gameState for safeZone flag in safeZoneSystem

--- src/rpg/systems/SafeZoneSystem.java
+++ src/rpg/systems/SafeZoneSystem.java
@@ -7,12 +7,12 @@ import rpg.game.GameState;
 public class SafeZoneSystem {
     public static void enterSafeZone(Player player, GameState state) {
         player.healFull();
+        state.inSafeZone = true; // 🆕 mark safe zone
         TextEffect.typeWriter("You feel renewed as you enter Safe Zone " + state.zone + ".", 60);
         TextEffect.typeWriter("Your stats have been fully restored.", 60);
     }
 
     public static void searchSafeZone(Player player, GameState state) {
-        // Placeholder for zone-based search logic; safe to keep empty for now
         TextEffect.typeWriter("You search around but find nothing useful.", 60);
     }
 }
