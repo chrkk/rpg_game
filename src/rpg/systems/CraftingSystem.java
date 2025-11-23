b157db46d23acb7ecf0285b2bcc2073ab783132b
chrkk <christianjake.alalim@cit.edu>
2025-11-23 02:39:34 +0000

    fixed crafting

--- src/rpg/systems/CraftingSystem.java
+++ src/rpg/systems/CraftingSystem.java
@@ -12,38 +12,35 @@ public class CraftingSystem {
             // Example requirements per weapon
             switch (target) {
                 case "Crystal Sword":
-                    if (player.getWeapon() != null && player.getWeapon().getName().equals("Pencil Blade")
-                            && crystals >= 5) {
+                    if (crystals >= 5) {
                         crystals -= 5;
                         player.equipWeapon(new Weapon("Crystal Sword", 20, 35, 0.10, 2.0));
                         state.stage2WeaponCrafted = true;
                         TextEffect.typeWriter("⚔️ You forged a Crystal Sword! Its edge gleams with power.", 60);
                     } else {
-                        TextEffect.typeWriter("You need a Pencil Blade and 5 crystals.", 60);
+                        TextEffect.typeWriter("You don't have enough crystals. (Need: 5, Have: " + crystals + ")", 60);
                     }
                     break;
 
                 case "Flame Axe":
-                    if (player.getWeapon() != null && player.getWeapon().getName().equals("Crystal Sword")
-                            && crystals >= 8) {
+                    if (crystals >= 8) {
                         crystals -= 8;
                         player.equipWeapon(new Weapon("Flame Axe", 25, 40, 0.15, 1.8));
                         state.stage3WeaponCrafted = true;
                         TextEffect.typeWriter("🔥 You forged a Flame Axe! It radiates intense heat.", 60);
                     } else {
-                        TextEffect.typeWriter("You need a Crystal Sword and 8 crystals.", 60);
+                        TextEffect.typeWriter("You don't have enough crystals. (Need: 8, Have: " + crystals + ")", 60);
                     }
                     break;
 
                 case "Shadow Bow":
-                    if (player.getWeapon() != null && player.getWeapon().getName().equals("Flame Axe")
-                            && crystals >= 10) {
+                    if (crystals >= 10) {
                         crystals -= 10;
                         player.equipWeapon(new Weapon("Shadow Bow", 15, 30, 0.20, 2.5));
                         state.stage4WeaponCrafted = true;
                         TextEffect.typeWriter("🌑 You forged a Shadow Bow! Shadows bend to your will.", 60);
                     } else {
-                        TextEffect.typeWriter("You need a Flame Axe and 10 crystals.", 60);
+                        TextEffect.typeWriter("You don't have enough crystals. (Need: 10, Have: " + crystals + ")", 60);
                     }
                     break;
 
